8ee5800a3ee26403bafb77959811919ccd6c2abe
losarwarameya <losarwarah@gmail.com>
2023-10-16 22:18:04 +0530

    installed supabase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,30 @@ import { useState, useRef, useEffect } from "react";
 import Task from "./Task";
 import EditTask from "./EditTask";
 
+import { createClient } from "@supabase/supabase-js";
+
 function App() {
+  const supabaseUrl = "https://yhgnykgkrhwkchpfbiez.supabase.co";
+
+  useEffect(() => {
+    // Initialize the Supabase client
+    const supabase = createClient(
+      supabaseUrl,
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloZ255a2drcmh3a2NocGZiaWV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0NzA4NzMsImV4cCI6MjAxMzA0Njg3M30.WZKIj8D2UT1tgDSQx8hRq4cwT48n7hgQv55PL5p1WYk"
+    );
+    // Fetch data from a table
+    const fetchData = async () => {
+      let { data, error } = await supabase.from("tasks").select("*");
+      if (error) {
+        console.error("Error fetching data:", error);
+      } else {
+        console.log(data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
   const [editIndex, setEditIndex] = useState(null);
@@ -11,7 +34,7 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTodos([e.target[0].value,...todos]);
+    setTodos([e.target[0].value, ...todos]);
     setInput("");
   };
 
@@ -37,7 +60,20 @@ function App() {
 
   useEffect(() => {
     inputRef.current.focus();
-  },[])
+  }, []);
+
+  // Save Todos to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  // Load Todos from local storage on mount
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
 
   return (
     <>
