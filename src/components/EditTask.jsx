b69588a4d003c36b8c538828da7479aff674a1cf
losarwarameya <losarwarah@gmail.com>
2023-10-18 21:40:33 +0530

    cleaned code

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useRef, useEffect } from "react";
 
 const EditTask = (props) => {
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Task = (props) => {
   const { task, index, handleDelete, handleEdit } = props;
 
